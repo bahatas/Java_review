7605ecaa09aa64f2a61bc4a44ce200f8b3319323
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-07-24 23:06:25 +0100

    LeetCode_

---
+++ src/main/java/com/cybertek/leetcode/array/KidswithGreatestNumberOfCandy.java
@@ -0,0 +1,57 @@
+package com.cybertek.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidswithGreatestNumberOfCandy {
+
+    public static void main(String[] args) {
+
+        int[] candiess = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+
+        System.out.println("kidsWithCandies2(candiess, extraCandies) = " + kidsWithCandies2(candiess, extraCandies));
+
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+
+        List<Boolean> result = new ArrayList<>();
+        int a = 0;
+        while (a < candies.length) {
+            for (int i = 0; i < candies.length; i++) {
+                if (candies[0] + extraCandies >= candies[i])
+                    result.add(true);
+
+            }
+        }
+        return null;
+    }
+
+    public static List<Boolean> kidsWithCandies2(int[] candies, int extraCandies) {
+
+
+        List<Boolean> result = new ArrayList<>();
+        int[] sortedArray = Arrays.copyOf(candies, candies.length);
+
+        Arrays.sort(sortedArray);
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= sortedArray[sortedArray.length - 1])
+                result.add(true);
+            else {
+                result.add(false);
+            }
+
+        }
+
+
+        return result;
+    }
+
+}
+
+
+
