e6342824159838d9d90c44877ec9f9798ad08f91
bahatas <baha@cybetekschool.com>
2021-07-24 18:50:59 +0100

    leetCodeArray

---
+++ src/main/java/com/cybertek/leetcode/KidswithGreatestNumberOfCandy.java
@@ -0,0 +1,19 @@
+package com.cybertek.leetcode;
+
+import java.util.List;
+
+public class KidswithGreatestNumberOfCandy {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+
+        return null;
+    }
+}
+
+
+
