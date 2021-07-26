ef3545b2c997ead6dbaf0daf7415964704364e78
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-07-26 23:49:00 +0100

    LeetCode_

---
+++ src/main/java/com/cybertek/leetcode/recursive/FibonacciRecursive.java
@@ -0,0 +1,16 @@
+package com.cybertek.leetcode.recursive;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FibonacciRecursive extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
