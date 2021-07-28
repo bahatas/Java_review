93951878312c59089dcb1d09a3c882d423ffa6cf
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-07-28 23:47:13 +0100

    LeetCode_Recursive Fibonacci

--- src/main/java/com/cybertek/leetcode/recursive/FibonacciRecursiveSolution.java
+++ src/main/java/com/cybertek/leetcode/recursive/FibonacciRecursiveSolution.java
@@ -1,12 +1,20 @@
 package com.cybertek.leetcode.recursive;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 public class FibonacciRecursiveSolution {
 
+
+    // 0 1 1 2 3 5 8 13 21 ...
     public static void main(String[] args) {
 
+        System.out.println(recursiveFib(7));
+    }
+
+
+    public static int recursiveFib(int number) {
+
+        if (number == 0) return 0;
+        if (number == 1) return 1;
+        return recursiveFib(number - 1) + recursiveFib(number - 2);
     }
 
 }
