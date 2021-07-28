2d076d383448614b53cb625915b00ddaf08ff652
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-07-28 23:39:16 +0100

    LeetCode_

--- src/main/java/com/cybertek/leetcode/recursive/FibonacciRecursiveSolution.java
+++ src/main/java/com/cybertek/leetcode/recursive/FibonacciRecursiveSolution.java
@@ -9,16 +9,4 @@ public class FibonacciRecursiveSolution {
 
     }
 
-    static int fib(int number){
-
-        if(number == 0) return 0;
-
-        else if (number == 1) return 1;
-
-        else {
-            return fib(number-1)+fib(number-2);
-        }
-
-        return null;
-    }
 }
