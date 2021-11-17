9fc0066255304b202886d5c3c7386528393a2d5d
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-11-17 14:31:30 +0000

    Algo12 Data structures

--- src/main/java/com/cybertek/leetcode/array/ALgo12_SmallestDifference.java
+++ src/main/java/com/cybertek/leetcode/array/ALgo12_SmallestDifference.java
@@ -1,6 +1,9 @@
 package com.cybertek.leetcode.array;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ALgo12_SmallestDifference {
@@ -9,6 +12,7 @@ public class ALgo12_SmallestDifference {
         int[] arrayOne = {-1, 5, 10, 20, 28, 3};
         int[] arrayTwo = {26, 134, 135, 15, 17};
         System.out.println("(getSmallestDiffPair(arrayOne,arrayTwo)) = " + Arrays.toString(getSmallestDiffPair(arrayOne, arrayTwo)));
+        Arrays.toString(getSmallestDiffPair2(arrayOne, arrayTwo));
     }
     static int[] getSmallestDiffPair(int[] firstArr, int[] secondArr) {
         HashMap<Integer, List<Integer>> hm = new HashMap<>();
@@ -28,9 +32,37 @@ public class ALgo12_SmallestDifference {
 
     static int[] getSmallestDiffPair2(int[] firstArr, int[] secondArr) {
 
-      int[] mergedArray = new int [firstArr.length+secondArr.length];
+        //put all in a hashmap with arraynumber
+        HashMap<Integer, List<Integer>> hmres = new HashMap<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        List list = new ArrayList(Arrays.asList(firstArr));
+        list.addAll(Arrays.asList(secondArr));
+
+        System.out.println("list "+list);
+        List < Object > array3 = new ArrayList < >();
+
+        Stream.of(firstArr, secondArr).flatMap(Stream::of).forEach(array3::add);
+
+        System.out.println(Arrays.toString(array3.toArray()));
+
+       // Arrays.stream(Stream.concat(Stream.of(firstArr),Stream.of(secondArr)).flatMapToInt(e->e)
+
+
+
+
+
+
+        // merge array
+
+        //put hashmap abstraction result and numbers
+
+        //sort them ==> compare with others :
+
+        int[] mergedArray = new int [firstArr.length+secondArr.length];
       Arrays.stream(firstArr).flatMap(each->Arrays.stream(secondArr));
-      //  Stream.of(firstArr,secondArr).flatMap(each->Arrays.stream())
+        Stream.of(firstArr, secondArr).flatMap(Stream::of).toArray();
+
 
 
         int[] result = new int[0];
@@ -44,6 +76,7 @@ public class ALgo12_SmallestDifference {
  * Description:
  * Smallest Difference
  * Difficulty: Medium Category: Arrays
+ *
  * Write a function that takes in two non-empty arrays of integers, finds the pair of numbers
  * (one from each array) whose absolute difference is closest to zero, and returns an array
  * containing these two numbers, with the number from the first array in the first position.
@@ -51,6 +84,7 @@ public class ALgo12_SmallestDifference {
  * number line. For example, the absolute difference of -5 and 5 is 10, and the absolute
  * difference of -5 and -4 is 1. You can assume that there will only be one pair of numbers with
  * the smallest difference.
+ *
  * Sample Input
  * arrayOne = [-1, 5, 10, 20, 28, 3]
  * arrayTwo = [26, 134, 135, 15, 17]
